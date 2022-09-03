9789adf26e1efcd159d67237fdded6f5e4ca368b
Rakarake <williambodin2001@gmail.com>
2022-09-03 15:30:58 +0200

    Initial commit

---
+++ client/html/src/org/megauno/game/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package org.megauno.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import org.megauno.game.Application;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Application();
+        }
+}
