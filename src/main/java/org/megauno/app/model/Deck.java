bac6cd15dae11b6f552f3a70d5b76b3e2d17e432
Lukas Gartman <gusgartlu@student.gu.se>
2022-09-15 19:17:59 +0200

    Created the Deck class

---
+++ src/main/java/org/megauno/app/model/Deck.java
@@ -0,0 +1,34 @@
+package org.megauno.app.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Stack;
+
+public class Deck implements IDeck {
+    private final Stack<ICard> cards = new Stack<>();
+    private HashMap<ICard, Integer> probabilities;
+    public Deck() {
+        // default distribution
+        this.probabilities = new HashMap();
+//        distribution.put(NumberCard, 66); // 66% chance for a NumberCard
+//        distribution.put(ActionCard, 33); // 33% chance for an ActionCard
+    }
+
+    public Deck(HashMap dist) {
+        this.probabilities = dist;
+    }
+
+    public ICard drawCard() {
+        ArrayList<ICard> cardProbs = new ArrayList<>();
+        for (ICard c : probabilities.keySet())
+            for (int i = 0; i < probabilities.get(c); i++)
+                cardProbs.add(c);
+
+        Random rand = new Random();
+        int index = rand.nextInt(cardProbs.size());
+        ICard randomCard = cardProbs.get(index);
+
+        return randomCard;
+    }
+}
---
+++ src/main/java/org/megauno/app/model/IDeck.java
@@ -0,0 +1,5 @@
+package org.megauno.app.model;
+
+public interface IDeck {
+    ICard drawCard();
+}
