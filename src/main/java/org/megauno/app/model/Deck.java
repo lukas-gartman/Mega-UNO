2e20bcfd2cc62c02b44d62f3be2d29a7753c986c
valtermiari <valter@miari.se>
2022-09-16 15:49:07 +0200

    Wired the Deck with Card-package

--- src/main/java/org/megauno/app/model/Deck.java
+++ src/main/java/org/megauno/app/model/Deck.java
@@ -1,5 +1,7 @@
 package org.megauno.app.model;
 
+import org.megauno.app.model.Cards.ICard;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
--- src/main/java/org/megauno/app/model/IDeck.java
+++ src/main/java/org/megauno/app/model/IDeck.java
@@ -1,5 +1,7 @@
 package org.megauno.app.model;
 
+import org.megauno.app.model.Cards.ICard;
+
 public interface IDeck {
     ICard drawCard();
 }
