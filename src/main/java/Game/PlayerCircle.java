8f1d166a74c06c2e917f1c198d0eecb79a87fabf
Clara Salberg <clarasalberg@live.com>
2022-09-16 15:54:30 +0200

    changes to PlayerCircle
    rename nextPlayer to nextTurn
    change return type of currentMakeTurn
    added variable for number of players currently in game
    added parameters to constructor

--- src/main/java/Game/PlayerCircle.java
+++ src/main/java/Game/PlayerCircle.java
@@ -1,5 +1,7 @@
 package Game;
 
+import java.util.List;
+
 // Circular doubly linked list to keep track of players and turns
 // dependency on Node class
 public class PlayerCircle {
@@ -8,9 +10,15 @@ public class PlayerCircle {
     private Node currentPlayer = null;
     rotation direction;
 
-    public PlayerCircle(){
+    private int numPlayers;
+
+    public PlayerCircle(List<Player> players){
         // starts with clockwise as default
         direction = rotation.CLOCKWISE;
+
+        for (Player p : players) {
+            addNode(p);
+        }
     }
 
     private void addNode(Player p) {
@@ -29,22 +37,40 @@ public class PlayerCircle {
 
         tail.nextNode = head;
         head.previousNode = tail;
+        numPlayers++;
+    }
+
+    private void removeNode(Node toRemove){
+        Node next = toRemove.nextNode;
+        Node prev = toRemove.previousNode;
+        prev.nextNode = next;
+        next.previousNode = prev;
+        numPlayers--;
     }
 
     // Changes currentPlayer to next in line depending on current rotation
-    public void nextPlayer(){
+    public void nextTurn(){
         if (direction == rotation.CLOCKWISE) currentPlayer = currentPlayer.nextNode;
         else currentPlayer = currentPlayer.previousNode;
     }
 
-    public void currentMakeTurn(){
-        currentPlayer.play();
+    public ICard currentMakeTurn(ICard top){
+       return currentPlayer.play(top);
     }
 
     public void changeRotation(){
         if (direction == rotation.CLOCKWISE) direction = rotation.ANTICLOCKWISE;
         else direction = rotation.CLOCKWISE;
     }
+
+    public int playersLeft(){
+        return numPlayers;
+    }
+
+    void returnCard(ICard choice){
+        currentPlayer.returnCard(choice);
+    }
+
 }
 
 enum rotation {
