0fc02de185b7fc515d8515b716b87242a29f8450
Lukas Gartman <gusgartlu@student.gu.se>
2022-09-17 13:52:34 +0200

    Generate default card probabilities

--- src/main/java/org/megauno/app/model/Deck.java
+++ src/main/java/org/megauno/app/model/Deck.java
@@ -1,20 +1,42 @@
 package org.megauno.app.model;
 
 import org.megauno.app.model.Cards.ICard;
+import org.megauno.app.model.Cards.ActionCard;
+import org.megauno.app.model.Cards.NumberCard;
+import org.megauno.app.model.Cards.Color;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Random;
-import java.util.Stack;
 
 public class Deck implements IDeck {
-    private final Stack<ICard> cards = new Stack<>();
     private HashMap<ICard, Integer> probabilities;
     public Deck() {
         // default distribution
         this.probabilities = new HashMap();
-//        distribution.put(NumberCard, 66); // 66% chance for a NumberCard
-//        distribution.put(ActionCard, 33); // 33% chance for an ActionCard
+
+        List<ICard> numberCards = generateNumberCards();
+        List<ICard> actionCards = generateActionCards();
+
+        for (ICard card : numberCards)
+            probabilities.put(card, 6);
+        for (ICard card : actionCards)
+            probabilities.put(card, 3);
+    }
+
+    private List<ICard> generateNumberCards() {
+        List<ICard> numberCards = new ArrayList<>();
+        for (Color colour : Color.values())
+            for (int num = 0; num <= 9; num++)
+                numberCards.add(new NumberCard(colour, num));
+        return numberCards;
+    }
+
+    private List<ICard> generateActionCards() {
+        List<ICard> actionCards = new ArrayList<>();
+        // todo: create action cards (need to create actions first)
+        return actionCards;
     }
 
     public Deck(HashMap dist) {
