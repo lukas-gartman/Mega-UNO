a26f6d817464068a3ec72b715c7cdf6af3a78e6a
KristofferGus <gusguskrp@student.gu.se>
2022-09-27 14:20:20 +0200

    Empty contruktor for Publisher

--- src/main/java/org/megauno/app/utility/ObserverPattern/Publisher.java
+++ src/main/java/org/megauno/app/utility/ObserverPattern/Publisher.java
@@ -1,12 +1,12 @@
 package org.megauno.app.utility.ObserverPattern;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Publisher<newsPaper> {
-    private List<Subscriber<newsPaper>> subscribers;
+    private List<Subscriber<newsPaper>> subscribers = new ArrayList<>();
 
-    public Publisher(List<Subscriber<newsPaper>> subscribers) {
-        this.subscribers = subscribers;
+    public Publisher() {
     }
     public void addSubscriber(Subscriber<newsPaper> sub){
         subscribers.add(sub);
