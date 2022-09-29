e9b4a32ee2fffacfc0331e3d603a7554bd7153f7
Clara Salberg <clarasalberg@live.com>
2022-09-29 17:07:34 +0200

    Fixing merge conflicts

--- src/main/java/org/megauno/app/Application.java
+++ src/main/java/org/megauno/app/Application.java
@@ -1,56 +1,27 @@
 package org.megauno.app;
 
-import org.megauno.app.model.Cards.ICard;
-import org.megauno.app.model.Game.PlayerCircle;
-import org.megauno.app.model.Player.Player;
-import org.megauno.app.utility.ObserverPattern.Publisher;
-import org.megauno.app.viewcontroller.*;
 import org.megauno.app.model.Game.Game;
 import com.badlogic.gdx.ApplicationAdapter;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.megauno.app.viewcontroller.GameViewsFactory.createGameViews;
-
 public class Application extends ApplicationAdapter {
+	ViewController viewController;
 	Game game;
-	int nrOfPlayers;
-
-	public Application(int nrOfPlayers) {
-		this.nrOfPlayers = nrOfPlayers;
-	}
-
+	
 	@Override
-	public void create() {
-		Player[] players = new Player[nrOfPlayers];
-		for(int i = 0; i < nrOfPlayers; i++){
-			players[i] = new Player(new ArrayList<>(),i);
-		}
-		PlayerCircle playerCircle = new PlayerCircle(Arrays.stream(players).toList());
-		Publisher<IGame> publisher = new Publisher<>();
-		game = new Game(publisher,playerCircle);
-
-		List<GameView> gameViews = createGameViews(players,publisher);
-
-
+	public void create () {
+		game = new Game();
+		viewController = new ViewController(game);
 	}
 
-	@Override
-	public void resize (int width, int height) {
-		// See below for what true means.
-		//viewController.resize(width, height);
-	}
 	@Override
 	public void render () {
-		//viewController.draw();
+		viewController.draw();
+		game.update();
 	}
 	
 	@Override
 	public void dispose () {
-		//viewController.teardown();
+		viewController.teardown();
 	}
 
 	public static void testFunc () {
--- src/main/java/org/megauno/app/model/Game/Game.java
+++ src/main/java/org/megauno/app/model/Game/Game.java
@@ -4,79 +4,162 @@ import org.megauno.app.model.Cards.ICard;
 import org.megauno.app.model.Deck;
 import org.megauno.app.model.Pile;
 import org.megauno.app.model.Player.Player;
-import org.megauno.app.utility.ObserverPattern.Publisher;
-import org.megauno.app.viewcontroller.IGame;
 
-public class Game implements IGame {
-	Publisher<IGame> gameChangePublisher;
-	PlayerCircle players;
-	// ICard top;
-	Deck deck;
-	Pile discarded;
+import java.util.List;
+import java.util.Random;
 
-	public Game(Publisher<IGame> publisher, PlayerCircle players) {
-		discarded = new Pile();
-		this.gameChangePublisher = publisher;
-		this.players = players;
-	}
+public class Game {
+    PlayerCircle players;
+    //ICard top;
+    Deck deck;
+    Pile discarded;
 
-	public void reverse() {
-		players.changeRotation();
-	}
+    public Game(){
+        this.discarded = new Pile();
+        this.deck = new Deck();
+    }
 
-	public void play() {
-		ICard top = discarded.getTop();
-		Node current = players.getCurrent();
-		ICard choice = players.currentMakeTurn(top);
-		boolean currentHasOnlyOneCard = current.getPlayer().numOfCards() == 1;
+    // For testing purposes
+    public Game(PlayerCircle players) {
+        this.discarded = new Pile();
+        this.deck = new Deck();
+        this.players = players;
+    }
 
-		if (choice.canBePlayed(top)) {
-			// card effects here ....
-
-			// change currentPlayer to next in line depending on game direction and position
-			// in circle:
-			players.nextTurn();
-
-			// discard played card
-			discarded.discard(choice);
-
-			// check if the player has run out of cards
-			if (players.playerOutOfCards(current)) {
-				// if the player only had one card, and never said uno,
-				if (currentHasOnlyOneCard && !current.getPlayer().uno()) {
-					// penalise: draw 3 cards.
-					current.giveCardToPlayer(deck.drawCard());
-					current.giveCardToPlayer(deck.drawCard());
-					current.giveCardToPlayer(deck.drawCard());
-				} else {
-					// removes player
-					players.playerFinished(current);
-				}
-			}
-
-			gameChangePublisher.publish(this);
-		} else {
-			players.giveCardToPlayer(choice);
+	// TODO: move method to a more general class (general class representing the model)
+	// commence_forth: set by a controller (or test) to signal that the player has chosen.
+	// tests should remember to call update
+	public boolean commence_forth = false;
+	public void update() {
+		if (commence_forth) {
+			try_play();
 		}
 	}
 
-	@Override
-	public Player getCurrent() {
-		return players.getCurrent().getPlayer();
-	}
+    public void reverse(){
+        players.changeRotation();
+    }
 
-	@Override
-	public Player getPlayerWithId(int id) {
-		for (Player p:getPlayers()) {
-			if(p.getId() == id){
-				return p;
-			}
-		}
-		return null;
-	}
+    private boolean validPlayedCards(List<ICard> play){
+        ICard top = discarded.getTop();
+        if(play.size() == 0){
+            return false;
+        }else if (!play.get(0).canBePlayed(top)){
+            return false;
+        }
+        for(int i = 1; i < play.size(); i++){
+            if(!play.get(i).canBeStacked(play.get(i-1))){
+                return false;
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public Player[] getPlayers() {
-		return players.getPlayers();
-	}
+    public void try_play() {
+        ICard top = discarded.getTop();
+        Node current = players.getCurrent();
+        List<ICard> choices = players.currentMakeTurn();
+        boolean currentHasOnlyOneCard = current.getPlayer().numOfCards() == 1;
+
+        if(validPlayedCards(choices)) {
+            // card effects here ....
+            for (ICard choice : choices) {
+                choice.activate();
+            }
+
+            // change currentPlayer to next in line depending on game direction and position in circle:
+            players.nextTurn();
+
+            // discard played card
+            for(int i = 0; i < choices.size(); i++){
+                discarded.discard(choices.get(i));
+            }
+
+
+            // check if the player has run out of cards
+            if (players.playerOutOfCards(current)) {
+                // if the player only had one card, and never said uno,
+                if (currentHasOnlyOneCard && !current.getPlayer().uno()) {
+                    //penalise: draw 3 cards.
+                    current.giveCardToPlayer(deck.drawCard());
+                    current.giveCardToPlayer(deck.drawCard());
+                    current.giveCardToPlayer(deck.drawCard());
+                } else {
+                    // removes player
+                    players.playerFinished(current);
+                }
+            }
+        }else{
+            for(int i = 0; i < choices.size(); i++) {
+                players.giveCardToPlayer(choices.get(i));
+            }
+        }
+
+    }
+
+    // To simulate a player choosing a card
+    public void currentPlayerChooseCard() {
+        Random rand = new Random();
+        Player currentPlayer = players.getCurrent().getPlayer();
+        int randomIndex = rand.nextInt(currentPlayer.numOfCards());
+        ICard randomCard = currentPlayer.getCards().get(randomIndex);
+        currentPlayer.selectCard(randomCard);
+    }
+
+    public boolean tryPlayTest() {
+        Node current = players.getCurrent();
+        currentPlayerChooseCard();
+        List<ICard> choices = players.currentMakeTurn();
+        boolean currentHasOnlyOneCard = current.getPlayer().numOfCards() == 1;
+        System.out.println("Current player: " + current.getPlayer().hashCode());
+        System.out.println("Player choices: " + choices);
+        System.out.println("Top card: " + discarded.getTop());
+
+        if (validPlayedCards(choices)) {
+            // card effects here ....
+            for (ICard choice : choices) {
+                choice.activate();
+            }
+            System.out.println("-------------<Successfully played>-------------");
+
+            // change currentPlayer to next in line depending on game direction and position in circle:
+            players.nextTurn();
+
+            // discard played card
+            for (ICard choice : choices) {
+                discarded.discard(choice);
+            }
+
+
+            // check if the player has run out of cards
+/*
+            if (players.playerOutOfCards(current)) {
+                // if the player only had one card, and never said uno,
+                if (currentHasOnlyOneCard && !current.getPlayer().uno()) {
+                    //penalise: draw 3 cards.
+                    current.giveCardToPlayer(deck.drawCard());
+                    current.giveCardToPlayer(deck.drawCard());
+                    current.giveCardToPlayer(deck.drawCard());
+                } else {
+                    // removes player
+                    players.playerFinished(current);
+                    System.out.println(players.playersLeft());
+                }
+            }
+*/
+            if (players.playerOutOfCards(current)) {
+                players.playerFinished(current);
+                System.out.println("Player " + current.getPlayer().hashCode() + " has finished");
+                System.out.println("Players left: " + players.playersLeft());
+            }
+        }
+        else {
+            for (ICard choice : choices) {
+                players.giveCardToPlayer(choice);
+            }
+            players.nextTurn();
+        }
+        System.out.println("\n|||||||||| New round |||||||||| \n");
+        return !current.equals(players.getCurrent());
+    }
 }
