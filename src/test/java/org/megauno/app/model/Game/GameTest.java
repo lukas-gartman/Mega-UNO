94de1fa772a8a82f4d1532a7014ad6fa5f10665c
valtermiari <valter@miari.se>
2022-10-03 13:23:13 +0200

    Method for dealing a hand implemented and tested.

---
+++ src/test/java/org/megauno/app/model/Game/GameTest.java
@@ -0,0 +1,18 @@
+package org.megauno.app.model.Game;
+
+import junit.framework.TestCase;
+
+public class GameTest extends TestCase {
+
+    Game game = new Game();
+
+    public void testUpdate() {
+        game.update();
+    }
+
+    public void testReverse() {
+    }
+
+    public void testTry_play() {
+    }
+}
