96de7aec47ed9d5f4e93784e08bf47ffdd924764
KristofferGus <gusguskrp@student.gu.se>
2022-10-05 16:36:03 +0200

    Sevice calss for ICard
    Added a new service class for ICard, a method for compying a list of cards as well as a method for fiding the difference between two lists of cards.

---
+++ src/main/java/org/megauno/app/utility/CardMethoodes.java
@@ -0,0 +1,32 @@
+package org.megauno.app.utility;
+
+import org.megauno.app.model.Cards.ICard;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardMethoodes {
+
+        public static ArrayList<ICard> copyCards (List<ICard> cards){
+            ArrayList<ICard> copy = new ArrayList<>();
+            for (ICard card: cards) {
+                copy.add(card.copyCard());
+            }
+            return copy;
+        }
+        public static List<ICard> cardsDifference(List<ICard> oldCards, List<ICard> newCards){
+            for (ICard oldCard: oldCards) {
+                for (int i = 0; i < newCards.size(); i++) {
+                    ICard card = newCards.get(i);
+                    if (oldCard.equals(card)){
+                        newCards.remove(card);
+                    }
+                }
+            }
+            return newCards;
+        }
+
+}
+
+
