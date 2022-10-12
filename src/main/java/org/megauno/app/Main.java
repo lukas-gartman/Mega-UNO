326a4519716fce6901305fb1cc919419edad1d8b
Lukas Gartman <lukas.gartman@gmail.com>
2022-10-12 23:40:41 +0200

    No need to minimize window for every run

--- src/main/java/org/megauno/app/Main.java
+++ src/main/java/org/megauno/app/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 public class Main {
     public static void main( String[] args ) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setMaximized(true);
+//        config.setMaximized(true);
         new Lwjgl3Application(new Application(), config);
     }
 }
