77a1755c838615bb179fcde289771b9cba72ff0d
valtermiari <valter@miari.se>
2022-10-17 18:39:45 +0200

    Moved canBeStackedUnder to the common interface ICardVisitor As well as documentation for Card domain

--- src/main/java/org/megauno/app/model/Cards/AbstractCard.java
+++ src/main/java/org/megauno/app/model/Cards/AbstractCard.java
@@ -4,11 +4,19 @@ import org.megauno.app.model.Cards.Impl.ActionCard;
 import org.megauno.app.model.Cards.Impl.NumberCard;
 import org.megauno.app.model.Game.IActOnGame;
 
+/**
+ * The common methods that a every type of card extends.
+ */
 public abstract class AbstractCard implements ICard {
 
     private Color color;
     private final CardType type;
 
+    /**
+     * Every card has a color and a type upon creation.
+     * @param color The specified color of the card.
+     * @param type The specified type of the card.
+     */
     protected AbstractCard(Color color, CardType type) {
         this.color = color;
         this.type = type;
@@ -29,17 +37,37 @@ public abstract class AbstractCard implements ICard {
 		return null;
 	}
 
+    /**
+     * Checks that this card can be played on the given card.
+     * @param card  The card that is played on.
+     * @return If the card can be played or not.
+     */
     @Override
-    public abstract boolean canBePlayed(ICard c);
+    public abstract boolean canBePlayed(ICard card);
 
+    /**
+     * Visitor Pattern. Checks that the given card can be played on this card.
+     * @param numberCard The card that wants to be played on this card
+     * @return Weather it can be played on this or not.
+     */
     @Override
-    public abstract boolean canBePlayedOnMe(NumberCard c);
+    public abstract boolean canBePlayedOnMe(NumberCard numberCard);
 
+    /**
+     * Visitor Pattern. Checks that the given card can be played on this card.
+     * @param actionCard The card that wants to be played on this card
+     * @return Weather it can be played on this or not.
+     */
     @Override
-    public abstract boolean canBePlayedOnMe(ActionCard ac);
+    public abstract boolean canBePlayedOnMe(ActionCard actionCard);
 
     public abstract ICard copyCard();
 
+    /**
+     * Propagates the changes a particular card has on the game.
+     * @param g An abstraction of a game.
+     * @return Weather the change was successfully done.
+     */
     @Override
     public abstract boolean activate(IActOnGame g);
 
--- src/main/java/org/megauno/app/model/Cards/ICardVisitor.java
+++ src/main/java/org/megauno/app/model/Cards/ICardVisitor.java
@@ -3,10 +3,38 @@ package org.megauno.app.model.Cards;
 import org.megauno.app.model.Cards.Impl.ActionCard;
 import org.megauno.app.model.Cards.Impl.NumberCard;
 
-// Defines some object that wants to interact with cards in some way
+/**
+ * An implementation of Visitor Pattern. This makes it possible to dynamically determine
+ * what particular type of card that is passed an argument. This behavior is desired since
+ * there are different rules to which type of card can be placed on another type of card.
+ */
 public interface ICardVisitor {
 
+    /**
+     * Checks that the given card can be played on this card.
+     * @param actionCard The card that wants to be played on this card
+     * @return Weather it can be played on this or not.
+     */
     boolean canBePlayedOnMe(ActionCard actionCard);
 
+    /**
+     * Checks that the given card can be played on this card.
+     * @param numberCard The card that wants to be played on this card
+     * @return Weather it can be played on this or not.
+     */
     boolean canBePlayedOnMe(NumberCard numberCard);
+
+    /**
+     * Checks that the given card can be stacked on this card.
+     * @param actionCard The card that wants to be stacked on this card
+     * @return Weather it can be stacked on this or not.
+     */
+    boolean canBeStackedUnder(ActionCard actionCard);
+
+    /**
+     * Checks that the given card can be stacked on this card.
+     * @param numberCard The card that wants to be stacked on this card
+     * @return Weather it can be stacked on this or not.
+     */
+    boolean canBeStackedUnder(NumberCard numberCard);
 }
